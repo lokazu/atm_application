1d4d3231d3b38606699c641b92c3bed4eb0db028
niran manandhar <nirnaism@gmail.com>
2016-09-05 17:21:14 -0500

    done with deposit checking

--- starter/js/atm.js
+++ starter/js/atm.js
@@ -8,10 +8,29 @@ $(document).ready(function() {
 //Checking account deposit function
 //On click of the depositChecking button
 //Get value from the amountChecking input field
+// Take that value and add it to the existing value in the checkingBalance div
+
 
   $('#depositChecking').on('click', function() {
-    var currentBalance = parseInt($('#checkingBalance').text().replace('$', ''));
+    var amountChecking = parseInt($('#amountChecking').val());
+    var checkingBalance = parseInt($('#checkingBalance').text().replace('$', ''));
+    var currentBalance = amountChecking + checkingBalance;
+    $('#checkingBalance').text('$'+ currentBalance);
+  });
+  //Savings account deposit function
 
+  //On click of the depositSavings button
+
+  //Get value from the amountSavings input field
+
+  //Take that value and add it to the existing value in the savingsBalance div
+
+  $('#depositChecking').on('click', function() {
+    var amountChecking = parseInt($('#amountChecking').val());
+    var checkingBalance = parseInt($('#checkingBalance').text().replace('$', ''));
+    var currentBalance = amountChecking + checkingBalance;
+    $('#checkingBalance').text('$'+ currentBalance);
+  });
 
     // console.log(currentBalance);
     // console.log('currentBalance');
@@ -29,41 +48,31 @@ $(document).ready(function() {
     // var y = $(depositChecking);
     // var checkingBalance = x+y;
 
-  });
+    // function replace(string) {
+    //   newString = parseInt(string, replace )
+    // }
+
+
+
+    // Take that value and add it to the existing value in the checkingBalance div
+    //Checking account withdrawl funtion
+    //On click of the withdrawChecking button
+    //Get value from the amountChecking input field
+    // If that value is greater than the value in the account ignore that action
+    // In other words if this would put the account into a negative balance do not allow it
+    //Else subtract that value from the current amount in the checking account
+
+
 
 });
 
 
 
-// function replace(string) {
-//   newString = parseInt(string, replace )
-// }
 
 
 
 
 
-
-// Take that value and add it to the existing value in the checkingBalance div
-
-//Checking account withdrawl funtion
-
-//On click of the withdrawChecking button
-
-//Get value from the amountChecking input field
-
-// If that value is greater than the value in the account ignore that action
-// In other words if this would put the account into a negative balance do not allow it
-
-//Else subtract that value from the current amount in the checking account
-
-//Savings account deposit function
-
-//On click of the depositSavings button
-
-//Get value from the amountSavings input field
-
-//Take that value and add it to the existing value in the savingsBalance div
 
 //Savings account withdraw funtion
 
